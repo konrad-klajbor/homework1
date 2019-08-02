c31c5c7048b47e78d0c27c5909d44f486a46f640
konrad-klajbor <konrad.klajbor@gmail.com>
2019-08-02 17:49:35 +0200

    Initial commit

---
+++ src/test/Main.java
@@ -0,0 +1,33 @@
+package test;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int liczba=((int)(Math.random()*9)+1);
+        boolean wygrana=false;
+
+        //System.out.println(liczba);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Witaj w zgadywance, wylosowałem liczbę z zakresu 1-10, spróbuj zgadnąć, powodzenia!");
+        do{
+            System.out.println("Podaj liczbę");
+
+            int proba = scanner.nextInt();
+            if (proba==liczba){
+                wygrana=true;
+            }
+            else {
+                System.out.println("Błąd! Spróbuj jeszcze raz...");
+            }
+        }while(!wygrana);
+        System.out.println("Gratulacje, wygrałeś!");
+
+
+
+
+        }
+    }
+
+
