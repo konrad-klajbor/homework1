25e396ca20d0e00fdf12b7937b39a9345e7768ef
konrad-klajbor <konrad.klajbor@gmail.com>
2019-08-02 19:59:47 +0200

    Added rounds counter

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+B
+src/test/Main.java,1\a\1afe47e5d1b73c17efc0a301cebed4fd15e4b596
--- src/test/Main.java
+++ src/test/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         int liczba=((int)(Math.random()*9)+1);
         boolean wygrana=false;
+        int rundy=0;
 
         //System.out.println(liczba);
         Scanner scanner = new Scanner(System.in);
@@ -20,9 +21,10 @@ public class Main {
             }
             else {
                 System.out.println("Błąd! Spróbuj jeszcze raz...");
+                rundy++;
             }
         }while(!wygrana);
-        System.out.println("Gratulacje, wygrałeś!");
+        System.out.println("Gratulacje, wygrałeś! Liczba rund: " + rundy);
 
 
 
